0bdcd2300dd5856df0ef8b7b08c3034c2b1de3f2
Nathan <nfaber@olin.edu>
2020-12-02 16:00:59 -0800

    Fixed broken launch files and added visualization for loss in validation and training sets for LSTM models MORE DATA NEEDED!!!!

--- ml_models/train_LSTM.py
+++ ml_models/train_LSTM.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np  # Matrix handling
 import tensorflow as tf
+from matplotlib import pyplot
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense
 
@@ -22,8 +23,8 @@ def split_sequences(sequences, n_steps):
 
 dataset_loc = os.path.expanduser("~/catkin_ws/src/ml_comprobofinal/datasets/")
 model_loc = os.path.expanduser("~/catkin_ws/src/ml_comprobofinal/ml_models/")
-dataset_name = "target_neato.npy"
-n_steps = 2
+dataset_name = "LSTM_05_20_002.npy"
+n_steps = 5
 
 if __name__ == "__main__":
     raw_data = np.load(dataset_loc + dataset_name)
@@ -38,16 +39,27 @@ if __name__ == "__main__":
     MAKE THE MODEL!!
     """
     model = Sequential()
-    model.add(LSTM(50, activation='relu', input_shape=(n_steps, n_features)))
+    model.add(LSTM(20, activation='relu', input_shape=(n_steps, n_features)))
     model.add(Dense(1))
-    model.compile(optimizer='adam', loss='mse')
+    model.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
 
     """
     Train the model!
     """
     # fit model
-    model.fit(input, output, epochs=1500, verbose=1)
+    #model.fit(input, output, epochs=1000, verbose=1)
+    history = model.fit(input, output, epochs=50, validation_split=0.2)
+    # plot train and validation loss
+    pyplot.plot(history.history['loss'][1:])
+    pyplot.plot(history.history['val_loss'][1:])
+    pyplot.title('model train vs validation loss')
+    pyplot.ylabel('loss')
+    pyplot.xlabel('epoch')
+    pyplot.legend(['train', 'validation'], loc='upper right')
+    pyplot.show()
 
     export_path_sm = model_loc + dataset_name.split('.')[0]
     print("Saving to: ", export_path_sm)
     tf.saved_model.save(model, export_path_sm)
+
+
